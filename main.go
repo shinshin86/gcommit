7753d9843b1102bd1566a137778e3f77404af01a
shinshin86 <beagles1986@gmail.com>
2023-03-23 22:27:52 +0900

    feat: integrate OpenAI chat API
    This commit adds a function that uses OpenAI's chat API to generate commit messages. It sends the git diff as a message and requests a response to be used as a commit message. This feature will greatly improve the efficiency and consistency of commit messages, ensuring they are always appropriately informative.

--- main.go
+++ main.go
@@ -1,20 +1,90 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"os/exec"
 	"strings"
 )
 
-func main() {
-	cmd := exec.Command("git", "diff")
-	output, err := cmd.CombinedOutput()
+const URL string = "https://api.openai.com/v1/chat/completions"
+
+type message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type chatRequest struct {
+	Model    string    `json:"model"`
+	Messages []message `json:"messages"`
+}
+
+type chatResponse struct {
+	Choices []struct {
+		Message message `json:"message"`
+	} `json:"choices"`
+}
+
+func chat(messages []message, apiKey string) string {
+	reqBody := chatRequest{
+		Model:    "gpt-3.5-turbo",
+		Messages: messages,
+	}
+	reqJSON, err := json.Marshal(reqBody)
 
 	if err != nil {
-		fmt.Printf("Error running git diff: %v\n", err)
-		return
+		log.Fatalln(err)
 	}
 
+	req, err := http.NewRequest("POST", URL, bytes.NewBuffer(reqJSON))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer res.Body.Close()
+
+	var resBody chatResponse
+	json.NewDecoder(res.Body).Decode(&resBody)
+
+	message := resBody.Choices[0].Message
+
+	return message.Content
+}
+
+func main() {
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		log.Fatalln("API key must be defined as an environment variable.")
+	}
+
+	cmd := exec.Command("git", "diff")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("Error running git diff: %v\n", err)
+	}
+
+	var messages []message
+	systemMsg := message{Role: "system", Content: "You are a specialist in thinking about git commit messages. Please generate a commit message based on the results of the git diff I'm about to send you."}
+	messages = append(messages, systemMsg)
+
 	diff := strings.TrimSpace(string(output))
-	fmt.Println(diff)
+	diffMsg := message{Role: "user", Content: diff}
+	messages = append(messages, diffMsg)
+
+	fmt.Println(chat(messages, apiKey))
 }
